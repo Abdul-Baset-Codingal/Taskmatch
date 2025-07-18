965c9c0832d276880b1240883cb8fb67d0d64d69
Abdul-Baset-Codingal <abdulbaset.codingal@gmail.com>
2025-07-19 03:14:35 +0600

    lets see

--- src/features/auth/authApi.ts
+++ src/features/auth/authApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://taskmatch-backend-hiza.onrender.com/api",
+        baseUrl: "https://taskmatch-backend-hiza.onrender.com/api/auth",
         credentials: "include", // ✅ This is correct
         prepareHeaders: (headers) => {
             // Ensure proper headers for CORS
